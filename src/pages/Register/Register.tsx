fbc5ff8357c7904442c63a30557e7c86f79947e2
Nguyen Vu <nguyenvu191000@gmail.com>
2023-01-20 14:43:13 +0700

    Login & Register page

--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,82 @@
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import Input from 'src/components/Input'
+import { schema, Schema } from 'src/utils/rules'
+
+type FormData = Pick<Schema, 'email' | 'password' | 'confirm_password'>
+const registerSchema = schema.pick(['email', 'password', 'confirm_password'])
+
 export default function Register() {
-  return <div>Register</div>
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+  return (
+    <div className='bg-orange'>
+      {/* <Helmet>
+        <title>Đăng ký | Shopee Clone</title>
+        <meta name='description' content='Đăng ký tài khoản vào dự án Shopee Clone' />
+      </Helmet> */}
+      <div className='container'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32 lg:pr-10'>
+          <div className='lg:col-span-2 lg:col-start-4'>
+            <form className='rounded bg-white p-10 shadow-sm' onSubmit={onSubmit} noValidate>
+              <div className='text-2xl'>Đăng ký</div>
+              <Input
+                name='email'
+                register={register}
+                type='email'
+                className='mt-8'
+                // errorMessage={errors.email?.message}
+                placeholder='Email'
+              />
+              <Input
+                name='password'
+                register={register}
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                // errorMessage={errors.password?.message}
+                placeholder='Password'
+                autoComplete='on'
+              />
+
+              <Input
+                name='confirm_password'
+                register={register}
+                type='password'
+                className='mt-2'
+                classNameEye='absolute right-[5px] h-5 w-5 cursor-pointer top-[12px]'
+                // errorMessage={errors.confirm_password?.message}
+                placeholder='Confirm Password'
+                autoComplete='on'
+              />
+
+              <div className='mt-2'>
+                {/* <Button
+                  className='flex w-full items-center justify-center bg-red-500 py-4 px-2 text-sm uppercase text-white hover:bg-red-600'
+                  isLoading={registerAccountMutation.isLoading}
+                  disabled={registerAccountMutation.isLoading}
+                >
+                  Đăng ký
+                </Button> */}
+              </div>
+              <div className='mt-8 flex items-center justify-center'>
+                <span className='text-gray-400'>Bạn đã có tài khoản?</span>
+                <Link className='ml-1 text-red-400' to='/login'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
