4e346850609e2e295e7f108a216df7570d96033b
Nguyen Vu <nguyenvu191000@gmail.com>
2023-01-30 19:51:31 +0700

    Handle expired token

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import useRouteElements from './hook/useRouteElements'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useContext, useEffect } from 'react'
+import { AppContext } from './contexts/app.context'
+import { LocalStorageEventTarget } from './utils/auth'
 
 function App() {
   const routeElements = useRouteElements()
+
+  const { reset } = useContext(AppContext)
+
+  useEffect(() => {
+    LocalStorageEventTarget.addEventListener('clearLS', reset)
+    return () => {
+      LocalStorageEventTarget.removeEventListener('clearLS', reset)
+    }
+  }, [reset])
+
   return (
     <div>
       {routeElements}
